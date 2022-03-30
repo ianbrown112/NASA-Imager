8ba395cf92df5b2bb020d2187ba2d3914f4d882c
Ian <ianbrown112@gmail.com>
2022-03-29 20:55:24 -0600

    Calendar fragment partially implemented

--- app/src/main/java/com/example/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.cst2335finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.btn1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                DialogFragment calendarFragment = new CalendarFragment();
+                calendarFragment.show(getSupportFragmentManager(),"Calendar Fragment");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
     }
 }
