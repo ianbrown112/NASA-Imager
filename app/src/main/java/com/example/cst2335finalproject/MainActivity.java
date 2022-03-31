321b2e70802cc49b051ca9c7665847e6fea76758
Ian <ianbrown112@gmail.com>
2022-03-30 19:44:23 -0600

    Calendar fragment works and updates TextView in MainActivity.java

--- app/src/main/java/com/example/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/MainActivity.java
@@ -9,15 +9,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private Button button;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        calendar = Calendar.getInstance();
         setContentView(R.layout.activity_main);
+        String defaultDate = DateFormat.getDateInstance().format(calendar.getTime());
+
+        TextView dateText = findViewById(R.id.selectedDate);
+        dateText.setText(defaultDate);
 
         button=findViewById(R.id.btn1);
 
@@ -31,8 +41,19 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         });
     }
 
+    /*Calendar code based on tutorial @ https://www.youtube.com/watch?v=33BFCdL0Di0&t=338s
+     "DatePickerDialog - Android Studio Tutorial" by Coding in Flow
+     Selecting date from calendar updates text box with given date
+     */
     @Override
-    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+    public void onDateSet(DatePicker view, int year, int month, int day) {
 
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        String selectedDate = DateFormat.getDateInstance().format(calendar.getTime());
+        TextView dateText = findViewById(R.id.selectedDate);
+        dateText.setText(selectedDate);
     }
 }
