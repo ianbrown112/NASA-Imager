02086a89b0b4393a9e5691883195e66347651268
Ian <ianbrown112@gmail.com>
2022-04-02 11:49:00 -0600

    Began introduction of ListView for favourites, adjusted sizing of image

---
+++ app/src/main/java/com/example/cst2335finalproject/FavouritesList.java
@@ -0,0 +1,14 @@
+package com.example.cst2335finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavouritesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites_list);
+    }
+}
