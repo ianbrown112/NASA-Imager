3cc2f47a9e5a37fb41f0f010e1c382d71c397046
Ian <ianbrown112@gmail.com>
2022-04-06 18:19:11 -0600

    Added activity for selected Image

---
+++ app/src/main/java/com/example/cst2335finalproject/SelectedImage.java
@@ -0,0 +1,14 @@
+package com.example.cst2335finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_image);
+    }
+}
